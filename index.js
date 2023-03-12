c3a36201fb20e769f2c5c33d48cf1cdfdb5993b7
Rayhan Shorker <raihanshorker56@gmail.com>
2023-03-12 22:56:07 +0600

    word translate in english

---
+++ index.js
@@ -0,0 +1,35 @@
+const handleSearch = () => {
+    const inputValue = document.getElementById("input-value").value;
+    if (inputValue) {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.title === "No Definitions Found") {
+            alert(data.message);
+          } else {
+            displayResult(data);
+          }
+        });
+    } else {
+      alert("empty input field ");
+    }
+  };
+  
+  const displayResult = (data) => {
+    const parent = document.getElementById("audio-container");
+    data[0].phonetics.forEach((element) => {
+      const audio = document.createElement("audio");
+      audio.src = element.audio;
+  
+      const button = document.createElement("button");
+      button.innerHTML = "Play";
+      button.onclick = () => {
+        audio.play();
+      };
+      const container = document.createElement("div");
+      container.appendChild(button);
+      container.appendChild(audio);
+  
+      parent.appendChild(container);
+    });
+  };
